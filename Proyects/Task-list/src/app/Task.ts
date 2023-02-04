9bdfdbef668aa79daeb08688747dc0b5f6f1cd38
leonls1 <110981915+leonls1@users.noreply.github.com>
2023-02-04 13:04:12 -0300

    Delete Proyects/Task-list directory

--- Proyects/Task-list/src/app/Task.ts
+++
@@ -1,9 +0,0 @@
-//to use my 'DB'
-import { TASKS } from "./mock-task";
-
-export interface Task{
-    id: number;
-    text: string;
-    day:string;
-    reminder:boolean
-}
--- Proyects/Task-list/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { OnInit } from '@angular/core';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
