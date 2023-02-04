a88519bc0222e0c6dee5fc1ef545b5c56fd211f1
leonls1 <leonlederhossturich@gmail.com>
2023-02-04 17:17:50 -0300

    added the main structure of cv, remains internal components of sub-components(xp, intro, education, etc)

--- Proyects/CV_Angular/src/app/app.module.ts
+++ Proyects/CV_Angular/src/app/app.module.ts
@@ -3,10 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpComponent } from './components/main-components/exp/exp.component';
+import { IntroComponent } from './components/main-components/intro/intro.component';
+import { EducationComponent } from './components/main-components/education/education.component';
+import { SkillsComponent } from './components/main-components/skills/skills.component';
+import { MainComponent } from './components/main/main.component';
+import { ProyectsComponent } from './components/main-components/proyects/proyects.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/header-components/nav-bar/nav-bar.component';
+import { OptionBarComponent } from './components/header-components/option-bar/option-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpComponent,
+    IntroComponent,
+    EducationComponent,
+    SkillsComponent,
+    MainComponent,
+    ProyectsComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent,
+    OptionBarComponent,
   ],
   imports: [
     BrowserModule,
