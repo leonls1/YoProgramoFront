5d10243af5814d819574fd976fc671a7c3131afc
leonls1 <leonlederhossturich@gmail.com>
2023-05-03 13:34:11 -0300

    adding angular, progress on introComponent

--- Proyects/CV_Angular/src/app/components/intro/intro.component.ts
+++ Proyects/CV_Angular/src/app/components/intro/intro.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class IntroComponent {
 
+  nombre: String = "Leon Lederhos Sturich";
+  titulo: String = "Full stack junior";
+  descripcion: String = "Estudiante de ingeniria en sistemas, desarrollando proyectos fullstack y aplicaciones .net";
+  editable: boolean = false;
+  validar(){
+    console.log("validado") 
+  }
+
+  enableEditing(){
+    this.editable = true;
+    console.log("editing");
+  }
 }
