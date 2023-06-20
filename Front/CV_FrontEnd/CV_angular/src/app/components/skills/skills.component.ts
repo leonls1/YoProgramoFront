14fc9a231a6845e043d3ea786659753c28271fca
leonls1 <leonlederhossturich@gmail.com>
2023-06-20 17:47:22 -0300

    finished LoggIn bloqued options

--- Front/CV_FrontEnd/CV_angular/src/app/components/skills/skills.component.ts
+++ Front/CV_FrontEnd/CV_angular/src/app/components/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Skill } from 'src/app/models/Skill.model';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 import { SkillService  } from 'src/app/service/skill.service';
 
 @Component({
@@ -9,8 +10,9 @@ import { SkillService  } from 'src/app/service/skill.service';
 })
 export class SkillsComponent implements OnInit{
   listaHabilidades :Skill[] =[];
+  loggedIn:boolean=false;
 
-  constructor(private servicio: SkillService){}
+  constructor(private servicio: SkillService, private auth: AuthenticationService){}
   
   ngOnInit(): void {
     this.obtenerHabilidades();
@@ -18,12 +20,12 @@ export class SkillsComponent implements OnInit{
 
   obtenerHabilidades(){
     this.servicio.obtenerListaHabilidades().subscribe(dato => {this.listaHabilidades = dato;});
-    
+    this.loggedIn = this.auth.isLoggedIn();
   }
 
   onDelete(id:number): void{
     if (id != undefined)
-    this.servicio.eliminarHabilidad(id).subscribe(data => {
+    this.servicio.eliminarHabilidad(id).subscribe(() => {
       this.obtenerHabilidades();
       alert("hablidad borrada")},
       error=>
